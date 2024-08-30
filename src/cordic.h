61c59a78cf613410c36bab0ee3bf276984c535f3
jouae <51120603+jouae@users.noreply.github.com>
2024-08-30 20:51:48 +0800

    Initial CORDIC header
    Implement four CORDIC functions
    
    Four types of CORDIC functions are introduced as following:
    
    1. Conventional CORDIC algorithm using float point
    
    2. Conventional CORDIC algorithm using fixed-point and a look up table
    
    3. Scale-free CORDIC algorithm
    	This method is based on approximating the cosine and sine functions
    	for small angles using the Taylor series.
    
    4. Hybird CORDIC algorithm
    	CORDIC base on a look up table and small angle approximation
    
    Refs:
    	1. Volder, Jack E. "The CORDIC trigonometric computing technique"
    	   IRE Transactions on electronic computers 3 (1959)
    
    	2. Dhar, Anindya S., and Swapna Banerjee.
    	   "An array architecture for fast computation of discrete Hartley transform"
    	    IEEE transactions on circuits and systems 38.9 (1991)
    
    	3. Arbaugh, Jason. "Table Look-up CORDIC: effective rotations
    	   through angle partitioning." (2004)

---
+++ src/cordic.h
@@ -0,0 +1,55 @@
+/*  
+ * A Coordinate Rotation DIgital Computer(CORDIC) C library
+ *
+ * Copyright (c) 2024 Yan-Jie, Chen
+ */
+
+typedef int32_t fixed_t;
+
+#define FIXED_INTEGER_BITS 23
+#define FIXED_FRACTION_BITS 8
+
+/*
+ * CORDIC algorithms
+ */
+
+/*
+ * Conventional CORDIC using floating point
+ *
+ * see
+ * Volder, Jack E. "The CORDIC trigonometric computing technique."
+ * IRE Transactions on electronic computers 3 (1959): 330-334.
+ */
+float conv_sin(float theta);
+float conv_cos(float theta);
+float conv_sincos(float theta);
+float conv_tan(float theta);
+
+/*
+ * CORDIC with look up table
+ */
+fixed_t LUT_sin(fixed_t theta);
+fixed_t LUT_cos(fixed_t theta);
+fixed_t LUT_sincos(fixed_t theta);
+fixed_t LUT_tan(fixed_t theta);
+
+/*
+ * Scale-free CORDIC
+ *
+ * see
+ * Dhar, Anindya S., and Swapna Banerjee. 
+ * "An array architecture for fast computation of discrete Hartley transform."
+ * IEEE transactions on circuits and systems 38.9 (1991): 1095-1098.
+ */
+fixed_t SF_sin(fixed_t theta);
+fixed_t SF_cos(fixed_t theta);
+fixed_t SF_sincos(fixed_t theta);
+fixed_t SF_tan(fixed_t theta);
+
+/*
+ * Hybird CORDIC
+ */
+fixed_t hybird_sin(fixed_t theta);
+fixed_t hybird_cos(fixed_t theta);
+fixed_t hybird_sincos(fixed_t theta);
+fixed_t hybird_tan(fixed_t theta);
